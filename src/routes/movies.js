49647be4828dff454d00b83883709ad9bebd4135
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-07-11 19:28:08 -0300

    16# le envío al cliente los datos de mi modelo de base de datos ...3000/api/movies

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -2,10 +2,10 @@ const {Router} = require ('express');
 const router = Router();
 
 const movies = require('../sample.json');
-console.log(movies);
+
 
 router.get('/', (req, res) =>{
-    res.send('movies');
+    res.json(movies);
 });
 
 
