6b55f73e4da94556ec81aab27c7b86e88c16970f
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-07-11 19:24:32 -0300

    15# obtengo los datos de mi modelo de base de datos y lo muestro por consola

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,9 @@
 const {Router} = require ('express');
 const router = Router();
 
+const movies = require('../sample.json');
+console.log(movies);
+
 router.get('/', (req, res) =>{
     res.send('movies');
 });
