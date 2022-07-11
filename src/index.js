17e653ff1878f6d5edcd1c9a19bf802230d86f86
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-07-11 16:12:16 -0300

    5# servidor configurado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require ('express');
 const app = express();
+const morgan = require ('morgan');
 
-app.listen(3000, ()=>{
+//settings
+app.set('port', 3000);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//starting the server
+app.listen(app.get('port'), ()=>{
   
-    console.log('server on port ${3000}');
+    console.log(`server on port ${app.get('port')}`);
 
 });
