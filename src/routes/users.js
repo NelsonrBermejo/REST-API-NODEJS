45174df8bf597daf72adeda2cea40fcd30b18f0a
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-07-11 22:40:43 -0300

    30# es una petición asincrona ya que llevará tiempo

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 const { Router} = require('express');
 const router = Router();
-
+const fetch = require('node-fetch');
 
 router.get('/', (req, res) =>{
+    fetch('https://jsonplaceholder.typicode.com/users');
     res.send('users');
 });
 
